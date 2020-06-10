8811f53943aa1af189a20ba572def83e18a19ec7
den-last <64039798+den-last@users.noreply.github.com>
2020-06-10 11:48:17 +0300

    Update script.js
    +слайдер

--- script/script.js
+++ script/script.js
@@ -2,4 +2,14 @@ $(document).ready(function() {
     $('.burger').click(function(event) {
         $('.burger,.head__menu').toggleClass('active');        
     });
+    $('.slider').slick({
+        arrows:true,
+        dots:false,
+        fade:false,
+        slidesToShow:1,
+        slidesToScroll:1,
+        speed:1000,
+        infinite:true,
+        autoplay:true,
+    });
 });
